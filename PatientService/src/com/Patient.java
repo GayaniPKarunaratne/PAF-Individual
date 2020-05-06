1051c4ea9179899a7aff805c99bfc6414918beab
GayaniPKarunaratne <gayanikarunaratne222@gmail.com>
2020-05-06 14:36:33 -0700

    Last Commit.Done all functions in video 10.

--- PatientService/src/com/Patient.java
+++ PatientService/src/com/Patient.java
@@ -1,4 +1,4 @@
-package model;
+package com;
 
 import java.sql.*;
 
@@ -6,15 +6,7 @@ public class Patient {
 	// A common method to connect to the DB
 	private Connection connect() {
 		Connection con = null;
-//		try
-//		{
-//			Class.forName("com.mysql.jdbc.Driver");
-//
-//			//Provide the correct details: DBServer/DBName, username, password
-//			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/patients", "root", "");
-//		}
-//		catch (Exception e)
-//		{e.printStackTrace();}
+
 
 		String url = "jdbc:mysql://localhost:3306/patients";
 		String username = "root";
@@ -37,7 +29,7 @@ public class Patient {
 				return "Error while connecting to the database for inserting.";
 			}
 			// create a prepared statement
-			String query = " insert into patients(patientName,patientAddress,patientAge,patientPhone)"
+			String query = " insert into patients(`patientName`,`patientAddress`,`patientAge`,`patientPhone`)"
 					+ "values (?, ?, ?, ? )";
 			PreparedStatement preparedStmt = con.prepareStatement(query);
 			// binding values
@@ -49,9 +41,13 @@ public class Patient {
 			// execute the statement
 			preparedStmt.execute();
 			con.close();
-			output = "Inserted successfully";
+			
+			String newPatients = readPatients();
+			output = "{\"status\":\"success\", \"data\": \"" + newPatients + "\"}";
+			
+			
 		} catch (Exception e) {
-			output = "Error while inserting the patient.";
+			output = "{\"status\":\"error\", \"data\":\"Error while inserting the patient.\"}";
 			System.err.println(e.getMessage());
 		}
 		return output;
@@ -69,6 +65,7 @@ public class Patient {
 			String query = "select * from patients";
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(query);
+			
 			// iterate through the rows in the result set
 			while (rs.next()) {
 				String patientID = rs.getString("patientID");
@@ -76,20 +73,16 @@ public class Patient {
 				String patientAddress = rs.getString("patientAddress");
 				String patientAge = rs.getString("patientAge");
 				String patientPhone = rs.getString("patientPhone");
+				
 				// Add into the html table
-				output += "<tr><td><input id=\"hidPatientIDUpdate\" name=\"hidPatientIDUpdate\"type=\"hidden\" value=\"" + patientID + "\">" + patientName + "</td>";
+				output += "<tr><td><input id='hidPatientIDUpdate' name='hidPatientIDUpdate' type='hidden' value='" + patientID + "'>" + patientName + "</td>";
 				output += "<td>" + patientAddress + "</td>";
 				output += "<td>" + patientAge + "</td>";
 				output += "<td>" + patientPhone + "</td>";
+				
 				// buttons
-				
-				
-
-				
-				output += "<td><input name=\"btnUpdate\" type=\"button\"value=\"Update\" class=\"btnUpdate btn btn-secondary\"></td>"
-						+ "<td><form method=\"post\" action=\"patients.jsp\">"
-						+ "<input name=\"btnRemove\" type=\"submit\" value=\"Remove\"class=\"btn btn-danger\">"
-						+ "<input name=\"hidPatientIDDelete\" type=\"hidden\" value=\"" + patientID + "\">"+ "</form></td></tr>";
+				output += "<td><input id='btnUpdate' name='btnUpdate' type='button' value='Update' class='btnUpdate btn btn-info'></td>"
+						+ "<td><input id='btnRemove' name='btnRemove' type='button' value='Remove' class='btnRemove btn btn-danger' data=patientid'"+ patientID + "'>"+ "</td></tr>";
 			}
 			con.close();
 			// Complete the html table
@@ -111,18 +104,22 @@ public class Patient {
 			// create a prepared statement
 			String query = "UPDATE patients SET patientName=?,patientAddress=?,patientAge=?,patientPhone=?WHERE patientID=?";
 			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
 			// binding values
 			preparedStmt.setString(1, name);
 			preparedStmt.setString(2, address);
 			preparedStmt.setString(3, age);
 			preparedStmt.setString(4, phone);
 			preparedStmt.setString(5, id);
+			
 			// execute the statement
 			preparedStmt.execute();
 			con.close();
-			output = "Updated successfully";
+			String newPatients = readPatients();
+			output = "{\"status\":\"success\", \"data\": \"" + newPatients + "\"}";
+			
 		} catch (Exception e) {
-			output = "Error while updating the patient.";
+			output = "{\"status\":\"error\", \"data\":\"Error while updating the patient.\"}";
 			System.err.println(e.getMessage());
 		}
 		return output;
@@ -138,14 +135,17 @@ public class Patient {
 			// create a prepared statement
 			String query = "delete from patients where patientID=?";
 			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
 			// binding values
 			preparedStmt.setString(1, patientID);
 			// execute the statement
 			preparedStmt.execute();
 			con.close();
-			output = "Deleted successfully";
+			
+			String newPatients = readPatients();
+			output = "{\"status\":\"success\", \"data\": \"" + newPatients + "\"}";
 		} catch (Exception e) {
-			output = "Error while deleting the patient.";
+			output = "{\"status\":\"error\", \"data\":\"Error while deleting the patient.\"}";
 			System.err.println(e.getMessage());
 		}
 		return output;
